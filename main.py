4e6458d5fbd2cb178a46f5a11bf2f261ec0c00e8
DevTiagold <tiagoaugustocostacarvalho@gmail.com>
2025-04-23 18:38:12 -0300

    Passo 5 completo - Visualizar os dados com gráficos

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-import scipy.io
+from scipy.io import loadmat, whosmat
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-
 
 # Load do Dataset_Grupo3.mat no diretório atual
 # O arquivo deve estar no mesmo diretório que este script ou você deve fornecer o caminho completo para o arquivo4
 
-data = scipy.io.loadmat('Dataset_Grupo3.mat')
+data = loadmat('Dataset_Grupo3.mat')
+data = data.get(whosmat('Dataset_Grupo3.mat')[0][0])[0][0]
 
-# A variável 'data' agora contém os dados do arquivo .mat
-# Você pode acessar os dados usando a chave correspondente no dicionário 'data'
-# Exemplo: se o arquivo contém uma variável chamada 'X', você pode acessá-la assim:
-# X = data['X']
-# Se você não souber os nomes das variáveis no arquivo, pode usar o seguinte código para listar todas as chaves:
-# for key in data.keys():
-#     print(key)
+Tempo, Entrada, Saida, QuantidadeFisica, Unidades = data
+Tempo = Tempo[0]
+Entrada = Entrada[0]
+Saida = Saida[0]
+QuantidadeFisica = QuantidadeFisica[0]
+Unidades = Unidades[0]
 
-print (data)
+plt.plot(Tempo, Entrada, label='Entrada')
+plt.plot(Tempo, Saida, label='Saída')
+plt.show()
+print(Tempo)
 
-# Se você quiser salvar os dados em um arquivo CSV, você pode usar o pandas
-# Primeiro, instale o pandas se ainda não o tiver instalado:
-# pip install pandas
+
+
+
+
+
+
+
+
+
 
 
 
